5df02f7e9f4803a9bfde5807b64ad2eeebef5ad8
Camdyn George <u3319879@uni.canberra.edu.au>
2026-03-19 13:20:33 +1100

    Tower of Hanoi Move Count

--- Tutorial 5/tower_of_hanoi.py
+++ Tutorial 5/tower_of_hanoi.py
@@ -1,19 +1,23 @@
+
 def main():
     n = eval(input("Enter number of disks: "))
     # Find the solution recursively
-    print("The moves are:")
-    moveDisks(n, 'A', 'B', 'C')
+    move_total = moveDisks(n, "A", "B", "C")
+    print("The moves are:", move_total)
+    
 
 # The function for finding the solution to move n disks
 # from fromTower to toTower with auxTower
 
 def moveDisks(n, fromTower, toTower, auxTower):
 
-    if n == 1: # Stopping condition
-        print("Move disk", n, "from", fromTower, "to", toTower)
+    if n == 1:
+        return 1
     else:
-        moveDisks(n - 1, fromTower, auxTower, toTower)
-        print("Move disk", n, "from", fromTower, "to", toTower)
-        moveDisks(n - 1, auxTower, toTower, fromTower)
+        return (
+            moveDisks(n - 1, fromTower, auxTower, toTower)
+            + 1
+            + moveDisks(n - 1, auxTower, toTower, fromTower)
+        )
 
 main() # Call the main function
